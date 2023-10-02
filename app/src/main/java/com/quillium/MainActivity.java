d461d35f2c989bef93d1809755837f2ee4556e3a
nishatrhythm <66105148+nishatrhythm@users.noreply.github.com>
2023-10-03 03:04:27 +0600

    Initial commit

--- app/src/main/java/com/quillium/MainActivity.java
+++ app/src/main/java/com/quillium/MainActivity.java
@@ -2,7 +2,12 @@ package com.quillium;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        // open login activity
+//        Button buttonLogin = findViewById(R.id.button_login);
+//        buttonLogin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//        // open register activity
+//        Button buttonRegister = findViewById(R.id.button_register);
+//        buttonRegister.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
