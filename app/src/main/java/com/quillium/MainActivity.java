a926ffd7f1fa3757a5d833fb9f360edda89c4f0d
nishatrhythm <66105148+nishatrhythm@users.noreply.github.com>
2023-10-03 13:40:43 +0600

    Initial commit

--- app/src/main/java/com/quillium/MainActivity.java
+++ app/src/main/java/com/quillium/MainActivity.java
@@ -1,13 +1,8 @@
 package com.quillium;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
