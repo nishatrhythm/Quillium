07e9ddd0ceb5dcbc9048f7d3073234e49ba0a6d4
nishatrhythm <66105148+nishatrhythm@users.noreply.github.com>
2023-10-05 10:58:16 +0600

    Initial commit

--- app/src/main/java/com/quillium/RegisterActivity.java
+++ app/src/main/java/com/quillium/RegisterActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
@@ -20,6 +21,16 @@ public class RegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // open verified data overview and verify email activity
+        Button Verify = findViewById(R.id.button_verify);
+        Verify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, VerifyEmail.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/quillium/VerifyEmail.java
@@ -0,0 +1,14 @@
+package com.quillium;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VerifyEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_email);
+    }
+}
